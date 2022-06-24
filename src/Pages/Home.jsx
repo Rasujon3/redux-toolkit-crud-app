297bae4320e5ec6b23b6c315118e2c1cf0336b7c
Rasujon3 <rasujon3@gmail.com>
2022-06-24 23:29:22 +0600

    final deploy done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Ruhul Amin Sujon </h2>
+      <p>
+        My goal has always been to be a Certified MERN Stack developer. Last 4-5
+        months, I spent most of my time learning React JS, and I'm aiming to
+        learning some new things in the upcoming days. I also hope to improve my
+        communication and public speaking skills. That will be helps me to join
+        the appropriate job sector as a full time MERN Stack developer. I really
+        want to be a proactive and valuable team member at my company, wherever
+        I work. I want to lead a team, so I will improve my leadership skills
+        and gain more experience.
+      </p>
     </div>
   );
 };
