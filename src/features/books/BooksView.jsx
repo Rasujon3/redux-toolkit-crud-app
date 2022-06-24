6d774527c8ce5073ab30e63af9f2126a7fe63f01
Rasujon3 <rasujon3@gmail.com>
2022-06-24 21:44:24 +0600

    delete book using redux toolkits

--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux/es/exports";
+import { useDispatch } from "react-redux";
+import { deleteBook } from "./BooksSlice";
 
 const BooksView = () => {
   const books = useSelector((state) => state.booksReducer.books);
-  console.log(books);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deleteBook(id));
+  };
   return (
     <div>
       <h2>List of books</h2>
@@ -27,7 +32,13 @@ const BooksView = () => {
                   <td>{author}</td>
                   <td>
                     <button>Edit</button>
-                    <button>Delete</button>
+                    <button
+                      onClick={() => {
+                        handleDelete(id);
+                      }}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               );
